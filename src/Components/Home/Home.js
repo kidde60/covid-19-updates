8aa1627a48619455ca430e9ada4a84b86ce767d6
kidde60 <kgeorgewilliam60@gmail.com>
2022-10-11 22:34:42 +0300

    Display fetched data

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,12 @@
+import React, { useEffect } from "react";
+import { useDispatch } from 'react-redux';
+import { fetchCountries } from '../../Redux/Home/Home'
+
 const Home = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(fetchCountries())
+    }, [])
     return (
         <div>
             home page

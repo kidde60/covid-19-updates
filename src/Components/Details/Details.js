d5189e7134806a9109dda6ea1de526d7056832ee
kidde60 <kgeorgewilliam60@gmail.com>
2022-10-13 08:26:22 +0300

    Add navigation

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,7 +1,33 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchCountries } from '../../Redux/Home/Home';
+import { useNavigate } from 'react-router-dom';
+
 const Details = () => {
+    const navigate = useNavigate();
+    const Detail = useSelector((state) => state.Home.countriesData)
+    // console.log(Detail)
+    const dispatch = useDispatch()
+    if (Detail.length == 0) {
+
+        dispatch(fetchCountries())
+    }
     return (
         <div>
-            Details page
+            {Detail.map((data) => (
+                <div key={data.ID}>
+                    <button type="button" onClick={() => { navigate("/"); }} >Home</button>
+                    <div>
+                        <h2>Country: {data.Country}</h2>
+                        <h2>Last Updated: {data.Date}</h2>
+                        <h2>New Confirmed Cases: {data.NewConfirmed}</h2>
+                        <h2>New Deaths: {data.NewDeaths}</h2>
+                        <h2>New Recovery : {data.NewRecovered}</h2>
+                        <h2>Total Confirmed Cases: {data.TotalConfirmed}</h2>
+                        <h2>Total Deaths: {data.TotalDeaths}</h2>
+                    </div>
+                </div>
+            ))}
         </div>
     );
 };

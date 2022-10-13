079f770b6731095352936578ff54ee2708ca4b7b
kidde60 <kgeorgewilliam60@gmail.com>
2022-10-13 08:26:39 +0300

    Add search bar

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,18 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCountries } from '../../Redux/Home/Home'
-
+import Details from "../Details/Details";
 const Home = () => {
     const countries = useSelector((state) => state.Home.countriesData);
     const global = useSelector((state) => state.Home.global);
-    console.log(countries)
-    console.log(global)
+    // console.log(countries)
+    // console.log(global)
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(fetchCountries())
     }, [dispatch])
+
+    const [searchTerm, setSearchTerm] = useState("")
     return (
-        <div>
+        <>
+            <div>
+
+                <input type="text" placeholder="Search Country ...."
+                    onChange={event => {
+                        setSearchTerm(event.target.value)
+                    }}
+                />
+            </div>
             <div>
                 <h2>Date: {global.Date}</h2>
                 <h2>New Confirmed: {global.NewConfirmed}</h2>
@@ -25,22 +35,30 @@ const Home = () => {
             {!countries.loading && countries.error ? <div>error{countries.error}</div> : null}
             {!countries.loading && countries.length ? (
                 <ul>
-                    {countries.map((country) => (
+                    {countries.filter((country) => {
+                        if (searchTerm == "") {
+                            return country
+                        } else if (country.Country.toLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+                            return country
+                        }
+                    }).map((country) => (
                         //  {TotalRecovered,  TotalDeaths,  TotalRecovered } = country
-                        <li key={country.ID}>
-                            <h2>{country.Country}</h2>
-                            <h3>Total Confirmed: {country.TotalConfirmed}</h3>
-                            <h3>Total Deaths: {country.TotalDeaths}</h3>
-                            <h3>New Confirmed: {country.NewConfirmed}</h3>
+                        <div key={country.ID}>
+                            <li>
+                                <h2>{country.Country}</h2>
+                                <h3>Total Confirmed: {country.TotalConfirmed}</h3>
+                                <h3>Total Deaths: {country.TotalDeaths}</h3>
+                                <h3>New Confirmed: {country.NewConfirmed}</h3>
 
 
 
-                        </li>
-
+                            </li>
+                            <button type="button">Details</button>
+                        </div>
                     ))}
                 </ul>
             ) : null}
-        </div>
+        </>
     )
 };
 

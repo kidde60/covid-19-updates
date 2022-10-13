591e0474c6178b102a308dc248cbcd381b5214bf
kidde60 <kgeorgewilliam60@gmail.com>
2022-10-14 01:26:44 +0300

    Add filter of a specific country

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,33 +1,41 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchCountries } from '../../Redux/Home/Home';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+
 
 const Details = () => {
     const navigate = useNavigate();
-    const Detail = useSelector((state) => state.Home.countriesData)
-    // console.log(Detail)
-    const dispatch = useDispatch()
-    if (Detail.length == 0) {
+    const Detail = useSelector((state) => state.Detail.countriesDetails)
+    console.log(Detail)
+    const { CountryId } = useParams();
 
-        dispatch(fetchCountries())
-    }
+    console.log(CountryId)
     return (
         <div>
-            {Detail.map((data) => (
-                <div key={data.ID}>
-                    <button type="button" onClick={() => { navigate("/"); }} >Home</button>
-                    <div>
-                        <h2>Country: {data.Country}</h2>
-                        <h2>Last Updated: {data.Date}</h2>
-                        <h2>New Confirmed Cases: {data.NewConfirmed}</h2>
-                        <h2>New Deaths: {data.NewDeaths}</h2>
-                        <h2>New Recovery : {data.NewRecovered}</h2>
-                        <h2>Total Confirmed Cases: {data.TotalConfirmed}</h2>
-                        <h2>Total Deaths: {data.TotalDeaths}</h2>
+
+            <div >
+                {Detail.filter((country) => {
+                    if (CountryId === country.Country) {
+                        return country
+                    }
+                }).map((data) => (
+                    <div key={data.ID}>
+                        <div>
+                            <button type="button" onClick={() => { navigate("/"); }} >Home</button>
+                            <h2>Country: {data.Country}</h2>
+                            <h2>Last Updated: {data.Date}</h2>
+                            <h2>New Confirmed Cases: {data.NewConfirmed}</h2>
+                            <h2>New Deaths: {data.NewDeaths}</h2>
+                            <h2>New Recovery : {data.NewRecovered}</h2>
+                            <h2>Total Confirmed Cases: {data.TotalConfirmed}</h2>
+                            <h2>Total Deaths: {data.TotalDeaths}</h2>
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))
+
+                }
+            </div>
+
         </div>
     );
 };

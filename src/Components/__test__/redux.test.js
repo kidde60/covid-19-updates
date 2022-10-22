89fafe09a6a463faddba8141a60cc2779f19a82e
kidde60 <kgeorgewilliam60@gmail.com>
2022-10-22 04:00:52 +0300

    Redux test

--- src/Components/__test__/redux.test.js
+++ src/Components/__test__/redux.test.js
@@ -1,12 +1,10 @@
 import store from './mock/configureStore';
-import { fetchCountries } from './mock/home';
+import { fetchCountries } from './mock/homeSlice';
 
-// import store from "./mock/store";
-// import { fetchCurrencies } from "./mock/forexslice";
-
-describe("test the store", () => {
-  it("check if the store is populated after getCountries action is dispatched", async () => {
-    await store.dispatch(fetchCountries());
-    expect(store.getState().Home.countriesData.NewConfirmed).toEqual(193);
-  });
+describe('test the store', () => {
+  it('check if the store is populated after getCountries action is dispatched',
+    async () => {
+      await store.dispatch(fetchCountries());
+      expect(store.getState().country.countriesData.Country).toEqual('Afghanistan');
+    });
 });
